960449927284a7fb417483d9eb91970e886c69ae
Geoffrey Challen <challen@illinois.edu>
2017-09-03 14:54:25 -0500

    Initial Lab2 commit.

---
+++ src/main/java/HelloGraphicalWorld.java
@@ -0,0 +1,17 @@
+import edu.illinois.cs.cs125.lib.Zen.Zen;
+
+/**
+ * An example Hello World program written using Zen Graphics.
+ */
+
+public class HelloGraphicalWorld {
+
+    /**
+     * Print "Hello, world!" to the display.
+     *
+     * @param unused unused input parameters.
+     */
+    public static void main(final String[] unused) {
+        Zen.drawText("Hello, world!", 100, 200);
+    }
+}
