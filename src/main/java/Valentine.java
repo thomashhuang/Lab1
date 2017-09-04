f2e39c15ec57d35413b25fac1d710aac64c19b74
Geoffrey Challen <challen@illinois.edu>
2017-09-04 17:20:33 -0500

    Fix see JavaDoc comments.

--- src/main/java/Valentine.java
+++ src/main/java/Valentine.java
@@ -5,8 +5,9 @@ import edu.illinois.cs.cs125.lib.zen.Zen;
 /**
  * Example program written using Zen Graphics.
  *
- * @see https://github.com/cs125-illinois/Zen
- * @see https://cs125-illinois.github.io/Zen/
+ * @see <a href="https://github.com/cs125-illinois/Zen">Zen on GitHub</a>
+ * @see <a href="https://cs125-illinois.github.io/Zen/">Zen Documentation</a>
+ * @see <a href="https://cs125.cs.illinois.edu/lab/1/#zen">Lab 1 Writeup</a>
  */
 public class Valentine {
     /**
